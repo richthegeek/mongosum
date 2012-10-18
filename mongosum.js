d51679e52b4dbd433e729669d0fc5d8ebde8c6ac
Richard Lyon <richthegeek@gmail.com>
2012-10-18 17:26:10 +0100

    almost

--- mongosum.js
+++ mongosum.js
@@ -140,10 +140,9 @@
     var _this = this;
     if (summary_change_count > 0) {
       return this.getSummary(function(err, full_summary) {
-        summary._length += full_summary._length;
-        console.log(full_summary._length, summary._length);
+        full_summary._length += summary._length;
         full_summary = merge_summary(full_summary, summary, options);
-        console.log(full_summary._length, summary._length);
+        console.log(full_summary.length);
         return _this.setSummary(full_summary, function() {
           return callback && callback(err, data);
         });
